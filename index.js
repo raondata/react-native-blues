e4773323d2a2058f834097af4addc36631f3d2fb
Jake Park <glow153@gmail.com>
2022-09-16 15:12:16 +0900

    added functions for events

--- index.js
+++ index.js
@@ -1,6 +1,36 @@
-
-import { NativeModules } from 'react-native';
+import { NativeEventEmitter, NativeModules } from 'react-native';
 
 const { RNBlues } = NativeModules;
+
+const eventEmitter = new NativeEventEmitter(RNBlues);
+const eventMap = {};
+
+RNBlues.removeListener = (eventName: String) => {
+  console.log('[RNBlues.removeListener]');
+  if (Object.keys(eventMap).includes(eventName)) {
+    eventMap[eventName].remove();
+    delete eventMap[eventName];
+  } else {
+    throw new Error(`${eventName} 이벤트는 react-native-blues에 등록되지 않았습니다.`);
+  }
+};
+
+RNBlues.on = (eventName: String, handler: Function) => {
+  console.log('[RNBlues.on]');
+  if (Object.keys(eventMap).includes(eventName)) {
+    console.log(`[RNBlues.on] event ${eventName} already registered.`);
+    eventMap[eventName].remove();
+  }
+  eventMap[eventName] = eventEmitter.addListener(eventName, handler);
+  return eventMap[eventName];
+};
+
+
+RNBlues.removeAllEvents = () => {
+  Object.entries(eventMap).forEach(([eventName, e]) => {
+    e.remove();
+    delete eventMap[eventName];
+  });
+};
 
 export default RNBlues;
